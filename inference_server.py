7e64c98e4a3161ef12ab9d62fc5b36b9a92805fb
Oneza Vhora <58053532+OnezaVhora@users.noreply.github.com>
2025-04-10 17:29:17 -0500

    Update inference_server.py

--- inference_server.py
+++ inference_server.py
@@ -1,7 +1,5 @@
 # Inference Server
 
-# install flask
-
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
@@ -13,7 +11,6 @@ app = Flask(__name__)
 # Load the pre-trained model
 model = tf.keras.models.load_model("best_model.keras")
 
-# Make the inference server
 @app.route('/summary', methods=['GET'])
 def model_summary():
     return jsonify({
@@ -25,21 +22,30 @@ def model_summary():
         "output_shape": model.output_shape,
     })
 
-@app.route('/inference', methods=['POST']) 
+@app.route('/inference', methods=['POST'])
 def inference():
-   try:
-       image_bytes= request.data
-       image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-       image = image.resize((128, 128))  
-       image_array = np.array(image) / 255.0  
-       image_array = np.expand_dims(image_array, axis=0)  
-       
-       prediction = model.predict(image_array)[0][0]  
-       label = "damage" if prediction > 0.5 else "no damage"
-       return jsonify({"prediction": label})
-   except Exception as e:
-       return jsonify({"error": str(e)}), 400
-   
+    try:
+        # Ensure an image file was sent
+        if 'image' not in request.files:
+            return jsonify({
+                'error': 'Invalid request; pass a binary image file as a multi-part form under the "image" key.'
+            }), 400
+
+        # Read image file
+        file = request.files['image']
+        image = Image.open(io.BytesIO(file.read())).convert("RGB")
+        image = image.resize((128, 128))
+        image_array = np.array(image) / 255.0
+        image_array = np.expand_dims(image_array, axis=0)
+
+        # Make prediction
+        prediction = model.predict(image_array)[0][0]
+        label = "damage" if prediction > 0.5 else "no_damage"
+
+        return jsonify({"prediction": label})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
-       
